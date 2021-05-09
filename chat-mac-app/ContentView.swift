36624d8ea428478375d99b488b2bcdaa5ed3e7ab
justin5562587 <justin5562587@gmail.com>
2021-05-09 14:09:47 +0800

    Initial Commit

---
+++ chat-mac-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chat-mac-app
+//
+//  Created by justin on 2021/5/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chat-mac-app/chat_mac_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_mac_appApp.swift
+//  chat-mac-app
+//
+//  Created by justin on 2021/5/9.
+//
+
+import SwiftUI
+
+@main
+struct chat_mac_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
